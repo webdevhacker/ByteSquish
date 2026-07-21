25734230322c003f40f038bb3f58c18252c8efd2
webdevhacker <i.kumar.sharan@gmail.com>
2026-07-21 13:27:30 +0530

    changes to dark theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -15,29 +14,27 @@ import TermsAndConditions from './pages/TermsAndConditions';
 
 function App() {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <Router>
-          <div className="min-h-screen flex flex-col bg-[var(--bg-color)] text-[var(--text-color)] font-sans selection:bg-sky-500/30">
-            <Navbar />
-            <main className="flex-grow container mx-auto px-4 pt-32 pb-12">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
-                <Route path="/verify" element={<VerifyEmail />} />
-                <Route path="/admin" element={<AdminDashboard />} />
-                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </Router>
-      </AuthProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen flex flex-col bg-[var(--bg-color)] text-[var(--text-color)] font-sans selection:bg-sky-500/30">
+          <Navbar />
+          <main className="flex-grow container mx-auto px-4 pt-32 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+              <Route path="/verify" element={<VerifyEmail />} />
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
